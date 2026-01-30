3ca8e821bcc2900d510f200ba29be292e90720f6
kondrse <kondor85@mail.ru>
2026-01-31 02:07:38 +0300

    moving

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,9 +3,9 @@ import { Link, Tabs } from 'expo-router';
 import React from 'react';
 import { Pressable } from 'react-native';
 
-import { useClientOnlyValue } from '../../components/useClientOnlyValue';
-import { useColorScheme } from '../../components/useColorScheme';
 import Colors from '../../constants/Colors';
+import { useClientOnlyValue } from '../../hooks/useClientOnlyValue';
+import { useColorScheme } from '../../hooks/useColorScheme';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
